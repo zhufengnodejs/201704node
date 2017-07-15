b43c1ab8e3074d74af2ad425a5a0d683ee47f638
zhufengnodejs <zhufengnodejs@126.com>
2017-07-15 10:33:31 +0800

    1.老的路由实现方式

---
+++ 1.express/1.http.js
@@ -0,0 +1,29 @@
+/**
+ * 用原生和http模块实现路由 根据客户端请求的方法和路径不同返回不同的内容
+ * GET /signup 返回注册
+ * GET /signin 返回登录
+ * 其它路径 返回页面未找到
+ **/
+//引入http模块
+let http = require('http');
+//创建一个http服务器,参数是一个请求监听函数，当服务器收到客户端的请求的时候会执行此监听函数
+//req=请求对象 res=响应对象
+//乱码的原因是编辑器的编码(utf-8)和浏览器的默认编码(GBK)不一样
+http.createServer(function(req,res){
+  //获取本次请求的方法
+  let method = req.method;
+  //获取本次请求的路径
+  let url = req.url;
+  //告诉客户端浏览器我的内容类型是html格式的，文件编码是utf-8
+  res.setHeader('Content-Type','text/html;charset=utf-8');
+  //当请求的方法是GET并且url地址是/signup的时候
+  if(method === 'GET' && url === '/signup'){
+    //写入响应体并且结束响应
+    res.end('注册');
+  }else if(method == 'GET' && url ==='/signin'){
+    res.end('登录');
+  }else {
+    res.end('页面未找到');
+  }
+
+}).listen(8080);
---
+++ 1.express/README.md
@@ -0,0 +1,27 @@
+## express
+是一个node服务器开发框架，可以让我们的开发工作变得更加的简单高效
+
+## 扩展资源地址
+### 官网
+http://expressjs.com/
+
+###github源码
+https://github.com/expressjs/expressjs.com
+
+## 文件夹的名称
+1. 不能是模块名 express
+2. 不能有空格
+3. 最好不要有中文
+
+## 安装
+```
+npm init -y
+npm install express --save
+```
+
+## 观察请求和响应对象内容
+```
+curl -v http://www.baidu.com
+
+```
+## 路由
