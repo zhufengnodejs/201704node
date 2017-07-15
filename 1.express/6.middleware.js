bbc3f4bd9ecf331e95189ec1646e7b5be5bb625f
zhufengnodejs <zhufengnodejs@126.com>
2017-07-15 15:29:11 +0800

    3.send

--- 1.express/6.middleware.js
+++ 1.express/6.middleware.js
@@ -3,6 +3,7 @@
  * 不管客户端访问什么路径，都在控制台打印出 方法名 路径
  **/
 let app = require('express')();
+let STATUS_CODES = require('_http_server').STATUS_CODES;
 /**
  * 中间件的作用是什么
  * 1. 编写一些公用的逻辑,所有的路由都需要处理的只需要写一次就够了
@@ -17,10 +18,32 @@ app.use(function(req,res,next){
 });
 //为响应对象添加send属性
 //send和end一样都会结束写入响应体，一旦调用之后，则不能再次调用write或end方法了 ,其实在 send方法里面会调用end方法
+/*let STATUS_CODE = {
+  200:'成功',
+  400:'客户端错误',
+  500:'服务器端错误'
+}*/
 app.use(function(req,res,next){
   //不管你传的是什么类型，都转成字符串，然后调用end方法
-   res.send = function(){
-
+   res.send = function(params){
+      //先取得参数的类型
+      let type = typeof params;
+      //对象 数组 数字(当成状态码来处理)
+      switch(type){
+        case 'object':
+          //如果参数的类型是对象的，需要先转成字符串
+          params =JSON.stringify(params);
+          break;
+        case 'number':
+          //设置响应的状态码为params 200 OK 500 Server Error
+          res.statusCode = params;
+          //然后再找到200对应的描述信息赋给params
+          params = STATUS_CODES[params];
+          break;
+        default:
+          params = params.toString();
+      }
+      res.end(params);
    }
    next();
 });
@@ -39,6 +62,6 @@ app.use(function(req,res,next){
   next();
 });
 app.get('/user',function(req,res){
-  res.send(["zfpx1",'zfpx2']);
+  res.send(404);
 });
 app.listen(8080);
