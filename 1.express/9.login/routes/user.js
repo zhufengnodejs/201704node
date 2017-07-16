57c9ee5f9dfbe2b315005427c5e91891f27c5d4c
zhufengnodejs <zhufengnodejs@126.com>
2017-07-16 10:33:13 +0800

    6.注册登录

--- 1.express/9.login/routes/user.js
+++ 1.express/9.login/routes/user.js
@@ -6,3 +6,24 @@ let express = require('express');
  * POST /user/signin
  * GET /user/welcome  欢迎
  **/
+//返回一个路由中间件
+let router = express.Router();
+//当请求路径是/user/signup的时候会执行第二个参数的函数
+router.get('/signup',function(req,res){
+  //渲染一个模板，返回一个空白的注册表单
+  //1参数是模板的相对路径，相对模板的根目录
+  res.render('signup');
+});
+router.post('/signup',function(req,res){
+  res.send('注册');
+});
+router.get('/signin',function(req,res){
+  res.send('登录')
+});
+router.post('/signin',function(req,res){
+  res.send('登录')
+});
+router.get('/welcome',function(req,res){
+  res.send('欢迎')
+});
+module.exports = router;
--- 1.express/9.login/server.js
+++ 1.express/9.login/server.js
@@ -6,6 +6,13 @@ let express = require('express');
  */
 //app是请求监听函数,当服务器收到客户端的请求后执行的函数
 let app = express();
+let path = require('path');
+//设置模板的类型，它可以决定添加的模板后缀
+app.set('view engine','html');
+//设置模板存放的根目录，根路径
+app.set('views',path.resolve('views'));
+//用来指定某种模板的渲染方法
+app.engine('.html',require('ejs').__express);
 //user是个变量，它的值指向的是模块的导出对象 module.exports
 let user = require('./routes/user');
 //如果请求的URL路径是以/user开头的话，会交由user中间件来进行匹配子路径
