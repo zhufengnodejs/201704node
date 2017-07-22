eb8c9340b483c12753ffd1a44c5d1d9c98345d9c
zhufengnodejs <zhufengnodejs@126.com>
2017-07-22 13:03:15 +0800

    mongodb保存功能

--- 6.mongoose/1.mongoose.js
+++ 6.mongoose/1.mongoose.js
@@ -1,6 +1,8 @@
 //mongoose可以实现对数据库集合的增删改查
 //1. 引入mongoose模块
 let mongoose = require('mongoose');
+//mongoose自己的promise库已经废弃了，需要你提供你的Promise库
+mongoose.Promise = Promise;//es6自带的promise
 //连接数据库 数据库的名字可以随意写，不需要事先创建好
 //monogdb://IP:端口号/数据库名称
 //2. 返回连接对象
@@ -44,14 +46,76 @@ let User = conn.model('User',UserSchema);
   }
 });*/
 //修改和查询
-let users = [];
+/*let users = [];
 for(let i=1;i<=10;i++){
   users.push({name:`zfpx${i}`,age:i})
 }
 //create方法还可以接收一个数组 docs是保存成功之后的数组
 User.create(users,function(err,docs){
   console.log(docs);
-});
+});*/
+//update指更新 1参数是更新的条件 2参数是更新后的值
+//$inc = increment 增加 在原来字段的基础上加1
+/*User.update({_id:'5972cc94f3ef0e1580666fa6'},{$inc:{age:5}},function(err,result){
+  //{ ok: 1, nModified: 1, n: 1 }
+  // 更新成功 实际修改的条数1 符合更新条件的条数
+  //如果更新前的值和更新后的值是一样的话，那么不进行实际更新
+  console.log(result);
+});*/
+//数据操作中 九分查一分修改
+//1参数里放的是条件
+//2.参数里放的是要显示或排除显示的字段 {字段名:1}表示只显示此字段
+//  {字段名:0} 表示只排除显示指定的字段
+// 只显示name  只排除age  home
+//Can't canonicalize query: BadValue Projection cannot have a mix of inclusion and exclusion.
+//0 exclusion 非0 inclusion
+
+//两个条件的或 $or
+// age >2 <2 >=2 <=2
+// age $gt $lt $gte $lte
+// 模糊匹配
+/*User.find({name:/zfpx/},function(err,result){
+  console.log(err);
+  //不管查找到多少条记录，返回的永远是数组
+  console.log(result);
+});*/
+
+let username ,password;
+//最多只返回一条，只要查找一条记录之后就立刻返回，不需要再继续往下查询了
+/*User.findOne({username,password},function(err,doc){
+  //doc或者返回null,或者是一个文档对象
+  if(err){
+    console.log(err);
+  }else{
+    if(doc){
+      console.log('登录成功');
+    }
+  }
+});*/
+
+/*User.findById('5972cc94f3ef0e1580666fa6',{name:1},function(err,doc){
+  //{ _id: 5972cc94f3ef0e1580666fa6, name: 'zfpx1', age: 112, __v: 0 }
+  console.log(doc);
+});*/
+
+let pageNum=2;//当前页码
+let pageSize = 3;//每页的条数
+//skip跳过指定的条数
+//limit指定返回的最多条数
+//DeprecationWarning: Mongoose: mpromise (mongoose's default promise library) is deprecated, plug in your own promise library instead
+//sort指定排序的字段 age:-1 表示降序 1表示升序
+//只有在调用exec的时候 才会真正向服务器发起查询
+User.find()
+  .limit(pageSize)
+  .sort({age:1})
+  .skip((pageNum-1)*pageSize)
+  .exec(function(err,docs){
+    console.log(docs);
+  })
+
+
+
+
 
 
 
